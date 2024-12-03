47ab744f576cc759826fdada115ef70d864c84e3
yejinkang <yejink0105@gmail.com>
2024-12-03 04:16:53 +0000

    evaluteHand added and step 3 started

--- Poker.hs
+++ Poker.hs
@@ -2,6 +2,7 @@
 module Poker where
     import System.Random
     import Data.List
+    import Data.Ord (comparing)
 
     -- Step1
     -- types 
@@ -17,7 +18,7 @@ module Poker where
     data Player = Player {name :: String, hand :: Hand, chip :: Chip, isDealer :: Bool}
 
     data GameState = GameState {activePlayers :: [Player], deck :: Deck, communityCards :: [Card], 
-    pot :: Chip, bets :: [Chip], dealer :: Player, sb :: Player, bb :: Player}
+    pot :: Chip, bets :: [Chip], dealer :: Int, sb :: Int, bb :: Int}
 
     {- createDeck: Create a deck in an order -}
     createdDeck :: Deck
@@ -53,8 +54,8 @@ module Poker where
         | ThreeOfAKind Rank
         | Straight Rank
         | Flush [Card]
-        | FullHouse [Rank] [Rank]
-        | FourOfAKind [Rank]
+        | FullHouse Rank Rank
+        | FourOfAKind Rank
         | StraightFlush Rank
         | RoyalFlush
         deriving (Show, Eq, Ord)
@@ -68,13 +69,13 @@ module Poker where
         | isFlush suits         = Flush hand
         | isStraight ranks      = Straight (highestRank hand)
         | isThreeOfAKind ranks  = ThreeOfAKind (head $ threeOfAKind ranks)
-        | isTwoPair ranks       = TwoPair (head firstPair) (head secondPair)
-        | isOnePair ranks       = OnePair (head firstPair)
+        | isTwoPair ranks       = TwoPair firstPair secondPair
+        | isOnePair ranks       = OnePair firstPair
         | otherwise             = HighCard (highestRank hand)
         where
             ranks = map snd hand
             suits = map fst hand
-            firstPair = head (twoOfAKind ranks)
+            firstPair = head (twoOfAKind ranks) --eg. Ace 
             secondPair = last (twoOfAKind ranks) 
 
     --Helper functions 
@@ -96,6 +97,7 @@ module Poker where
             ranks = map snd hand
             suits = map fst hand 
 
+    --Helper functions 
     isOnePair :: [Rank] -> Bool
     isOnePair ranks = length (twoOfAKind ranks) == 1
 
@@ -103,21 +105,20 @@ module Poker where
     isTwoPair ranks = length (twoOfAKind ranks) == 2
 
     isFourOfAKind :: [Rank] -> Bool
-    isFourOfAKind ranks = not (null $ fourOfAKind ranks)
+    isFourOfAKind ranks = length (fourOfAKind ranks) == 1
 
     isThreeOfAKind :: [Rank] -> Bool
     isThreeOfAKind ranks = not (null $ threeOfAKind ranks)
 
     isFullHouse :: [Rank] -> Bool
-    isFullHouse ranks = length (twoOfAKind ranks) == 1 && threeOfAKind ranks == 1
-
+    isFullHouse ranks = length (twoOfAKind ranks) == 1 && length (threeOfAKind ranks) == 1
 
     --Helper functions 
     highestRank :: Hand -> Rank
     highestRank hand = maximum (map snd hand)
 
     --Sort and group by rank into lists and filter lists which their length is 2
-    twoOfAKind :: [Rank] -> [Rank]
+    twoOfAKind :: [Rank] -> [Rank] --eg. [Two, Ace]
     twoOfAKind = map head . filter (\x -> length x == 2) . group . sort
 
     threeOfAKind :: [Rank] -> [Rank]
@@ -126,13 +127,27 @@ module Poker where
     fourOfAKind :: [Rank] -> [Rank]
     fourOfAKind = map head . filter (\x -> length x == 4) . group . sort -- takes only head from the sublist 
 
+    {-determineWinner: determines the winner between multiple players based on their hand rankings-}
+    determineWinner :: [Player] -> [Player]
+    determineWinner players = 
+        let playerHands = [(p, evaluateHand (hand p)) | p <- players]
+            bestHand = maximumBy (comparing snd) playerHands
+            winners = filter ((== snd bestHand) . snd) playerHands --get all players with equal best hands
+        in map fst winners  
+
+    -- Step3
+    data Strategy = Bet | Call | Raise | Fold
+
+    bettingRound :: GameState -> [Player] -> GameState -> [Player] --maybe GameState wouldn't be an input yet just input pot
+    bettingRound = 
+        --call initialising functions ??
 
 
-    {-initialisePlayer: Initialise the Player-}
+    {-initialisePlayer: Initialises the Player-}
     initialisePlayer :: String -> Chip -> Player 
     initialisePlayer name chip = 
         let name' = name 
-            hand' = ([],[])
+            hand' = []
             chip' = chip
             isDealer' = False
         in Player{name = name, hand = hand', chip = chip, isDealer = isDealer'}
@@ -145,10 +160,20 @@ module Poker where
             communityCards' = []
             pot' = 0
             bets' = []
-            dealer' = players !! n
-            sb' = players !! (n+1)
-            bb' = players !! (n+2)
+            dealer' = 0 --have to be decided randomly later 
+            sb' = dealer' + 1
+            bb' = dealer' + 2
         in GameState {activePlayers = players, deck = deck', communityCards = communityCards', pot = pot',
                     bets = bets', dealer = dealer', sb = sb', bb = bb'}
+
+    main :: IO ()
+    main = do
+        let player1 = Player "Alice" [(Hearts, Ace), (Spades, Ace), (Diamonds, King), (Clubs, Queen), (Hearts, Ten)] 100 False
+        let player2 = Player "Bob" [(Clubs, King), (Diamonds, King), (Spades, Queen), (Hearts, Jack), (Spades, Ten)] 100 False
+        let player3 = Player "Charlie" [(Diamonds, Ten), (Clubs, Ten), (Hearts, Nine), (Spades, Eight), (Diamonds, Seven)] 100 False
+        let players = [player1, player2, player3]
+
+        putStrLn $ "Winner(s): " ++ show (map name (determineWinner players))
+
 
 
